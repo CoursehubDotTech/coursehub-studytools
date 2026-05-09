3d39a98ed6d588779c5039a34cbc49e21bc44a76
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-05-09 09:50:01 +0000

    removed old code

--- app/[locale]/courses/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import * as clerk from '@clerk/nextjs/server';
-import Course from "../../../pages/Course";
-
-export default async function CourseRoute({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return (
-    <Course courseId={id} />
-  );
-}
